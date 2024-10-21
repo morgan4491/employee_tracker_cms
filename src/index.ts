0748edb606629e08317306fc19eb6ae21afc90c0
morgan4491 <m.morgan4491@gmail.com>
2024-10-21 18:35:52 -0400

    started index.ts file and installed npm packages

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import pg from 'pg';
+import 'console.table';
+
+const { Client } = pg;
+const client = new Client({
+    user: 'postgres',
+    password: 'pass',
+    database: 'employee_tracker_db'
+});
+
+await client.connect();
