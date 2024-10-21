77c5154a2527f1c0b3575ddcd6f9542fcf70bf6b
morgan4491 <m.morgan4491@gmail.com>
2024-10-21 18:23:22 -0400

    started sql schema and seed files

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+\c postgres;
+
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+\c employee_tracker_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+    FOREIGN KEY (department_id) REFERENCES department (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id)
+        ON DELETE CASCADE
+    manager_id INT
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+        ON DELETE SET NULL
+);
